f061ac206c8443662bd194b8a753db025db2f0fa
Miha Rataj <rataj.miha@gmail.com>
2013-03-10 15:36:56 +0100

    added separate app

---
+++ BBCodeStringApplication/BBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBAppDelegate.h
+//  BBCodeStringApplication
+//
+//  Created by Miha Rataj on 10. 03. 13.
+//  Copyright (c) 2013 Miha Rataj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BBCodeStringApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BBCodeStringApplication
+//
+//  Created by Miha Rataj on 10. 03. 13.
+//  Copyright (c) 2013 Miha Rataj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BBAppDelegate class]));
+    }
+}
